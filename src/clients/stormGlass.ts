96be648e60542902afeee0358a3807e7662e6415
TairineEllen <tairine.ellen@gmail.com>
2021-11-20 14:51:41 -0300

    add parameter windSpeed

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,7 +1,7 @@
 import { AxiosStatic } from 'axios';
 
 export class StormGlass {
-  readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection';
+  readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassAPISource = 'noaa';
   constructor(protected request: AxiosStatic) {}  
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
