c7cddf81a9979cc5d908a8da262f9eb111c3e08d
TairineEllen <tairine.ellen@gmail.com>
2021-11-20 15:29:17 -0300

    add fixtures with stormGlass response

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
+import stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
 
@@ -8,10 +10,10 @@ describe('StormGlass client', () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue({});
+    axios.get = jest.fn().mockResolvedValue(stormglassWeatherPointFixture);
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
+    expect(response).toEqual(stormglassNormalizedResponseFixture);
   });
 });
