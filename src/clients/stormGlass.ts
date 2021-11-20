c85b02905277bc7598d20640dfa5970a8f82918b
TairineEllen <tairine.ellen@gmail.com>
2021-11-20 16:15:43 -0300

    add method to normalize the response

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -36,11 +36,25 @@ export class StormGlass {
   readonly stormGlassAPISource = 'noaa';
   constructor(protected request: AxiosStatic) {}  
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    const response = this.request.get<StormGlassForecastResponse>(
+    const response = await this.request.get<StormGlassForecastResponse>(
       `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`
-      );    
+      );
+      return this.normalizeResponse(response.data);
   }
   
+  private normalizeResponse(points: StormGlassForecastResponse): ForecastPoint[] {
+    return points.hours.filter(this.isValidPoint.bind(this)).map(point => ({
+      time: point.time,
+      swellDirection: point.swellDirection[this.stormGlassAPISource],
+      swellHeight: point.swellHeight[this.stormGlassAPISource],
+      swellPeriod: point.swellPeriod[this.stormGlassAPISource],
+      waveDirection: point.waveDirection[this.stormGlassAPISource],
+      waveHeight: point.waveHeight[this.stormGlassAPISource],
+      windDirection: point.windDirection[this.stormGlassAPISource],
+      windSpeed: point.windSpeed[this.stormGlassAPISource]
+    }));
+
+  }
 
   private isValidPoint(point: Partial<StormGlassPoint>) :boolean {
     return !! (
